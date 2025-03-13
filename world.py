f333dd1f13dd81f5c0b06e91f4763e2f33171bc9
DanielWhitehurst02 <dwhitehurst18@gmail.com>
2025-03-13 15:34:13 +1100

    Grid from background function

--- world.py
+++ world.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import pygame
 import math
-from queue import PriorityQueue
+import numpy as np
+from pathfinding.core.grid import Grid
 
 from sys import exit
 from settings import *
@@ -45,138 +46,60 @@ clock = pygame.time.Clock()
 # pygame.PixelArray.close(pxarray)
 
 
-# print(background.get_at((0,0)))
+##get pixel color
+print(background.get_at((0,0)))
+print(background.get_at((0,0)) == BLACK)
+
 
 background.set_at((0,0), RED)
 
-class Spot:
- def __init__(self, row, col, width, total_rows):
-  self.row = row
-  self.col = col
-  self.x = row * width
-  self.y = col * width
-  self.color = WHITE
-  self.neighbors = []
-  self.width = width
-  self.total_rows = total_rows
-
- def get_pos(self):
-  return self.row, self.col
-
- def is_closed(self):
-  return self.color == RED
-
- def is_open(self):
-  return self.color == GREEN
-
- def is_barrier(self):
-  return self.color == BLACK
-
- def is_start(self):
-  return self.color == ORANGE
-
- def is_end(self):
-  return self.color == TURQUOISE
-
- def reset(self):
-  self.color = WHITE
-
- def make_start(self):
-  self.color = ORANGE
-
- def make_closed(self):
-  self.color = RED
-
- def make_open(self):
-  self.color = GREEN
-
- def make_barrier(self):
-  self.color = BLACK
-
- def make_end(self):
-  self.color = TURQUOISE
-
- def make_path(self):
-  self.color = PURPLE
-
- def draw(self, win):
-  pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
-
- def update_neighbors(self, grid):
-  self.neighbors = []
-  if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier(): # DOWN
-   self.neighbors.append(grid[self.row + 1][self.col])
-
-  if self.row > 0 and not grid[self.row - 1][self.col].is_barrier(): # UP
-   self.neighbors.append(grid[self.row - 1][self.col])
-
-  if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier(): # RIGHT
-   self.neighbors.append(grid[self.row][self.col + 1])
-
-  if self.col > 0 and not grid[self.row][self.col - 1].is_barrier(): # LEFT
-   self.neighbors.append(grid[self.row][self.col - 1])
-
- def __lt__(self, other):
-  return False
-
-def h(p1, p2):
- x1, y1 = p1
- x2, y2 = p2
- return abs(x1 - x2) + abs(y1 - y2)
-
-def reconstruct_path(came_from, current, draw):
- while current in came_from:
-  current = came_from[current]
-  current.make_path()
-  draw()
-
-def pathfinding(draw, grid, start, end):
- count = 0
- open_set = PriorityQueue()
- open_set.put((0, count, start))
- came_from = {}
- g_score = {spot: float("inf") for row in grid for spot in row}
- g_score[start] = 0
- f_score = {spot: float("inf") for row in grid for spot in row}
- f_score[start] = h(start.get_pos(), end.get_pos())
-
- open_set_hash = {start}
-
- while not open_set.empty():
-  for event in pygame.event.get():
-   if event.type == pygame.QUIT:
-    pygame.quit()
-
-  current = open_set.get()[2]
-  open_set_hash.remove(current)
-
-  if current == end:
-   reconstruct_path(came_from, end, draw)
-   end.make_end()
-   return True
-
-  for neighbor in current.neighbors:
-   temp_g_score = g_score[current] + 1
-
-   if temp_g_score < g_score[neighbor]:
-    came_from[neighbor] = current
-    g_score[neighbor] = temp_g_score
-    f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
-    if neighbor not in open_set_hash:
-     count += 1
-     open_set.put((f_score[neighbor], count, neighbor))
-     open_set_hash.add(neighbor)
-     neighbor.make_open()
-
-  draw()
-
-  if current != start:
-   current.make_closed()
- return False
-
-
 def creategrid(background: pygame.Surface, width):
-    for i in range(background.get_width,
+    pxwidth = background.get_width()
+    pxheight = background.get_height()
+
+    # print(pxwidth)
+    # print(pxheight)
+
+    gridwd = int(pxwidth/robwidth)
+    gridhi = int(pxheight/robwidth) 
+    
+    grid = np.zeros(shape=(gridwd, gridhi))
+
+    print(grid.shape)
+    print(gridwd, gridhi)
+
+    for i in range(gridwd):
+      # print(i)
+      for j in range(gridhi):
+        #0
+        for k in range(robwidth):
+          for l in range(robwidth):
+            # background.get_at((i+k,j+l))
+            if background.get_at(((i*robwidth)+k,(j*robwidth)+l)) == BLACK:
+              grid[i,j] = 1
+              break
+        else:
+          continue  # only executed if the inner loop did NOT break
+        break
+
+    # print(grid)
+    return grid
+def drawgrid( win, grid, width):
+
+  for i in range(grid.shape[0]):
+    for j in range(grid.shape[1]):
+      if grid[i,j] == 0:
+        color = WHITE
+      elif grid[i,j] == 1:
+        color = BLACK 
+      else:
+        color = WHITE
+
+      pygame.draw.rect(win, color, (width*i, width*j, width, width))
+
+       
+
+
 
 
 #Robot
@@ -190,12 +113,21 @@ class Robot(pygame.sprite.Sprite):
 
 
 
-robwidth = 20
+robwidth: int = 10
 
 robot = Robot(robwidth)
+grid = creategrid(background,robwidth)
+screen.blit(background,(0,0))
+drawgrid( screen, grid, robwidth)
 
+goal = [32,24]
+
+# pygame.draw.rect(screen, c, (width*i, width*j, width, width))
+
+pygame.display.update()
 
 while True:
+    
     keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -203,7 +135,7 @@ while True:
             exit()
     # screen.fill(white)
 
-    screen.blit(background,(0,0))
+    
 
     screen.blit(robot.image,robot.pos)
 
